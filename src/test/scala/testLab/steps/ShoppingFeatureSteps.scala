ad3bfd4ced14e524e10812066e5889f13068e876
Maria Mengozzi <maria.mengozzi3@studio.unibo.it>
2022-05-04 11:19:22 +0200

    implement ex06

--- src/test/scala/testLab/steps/ShoppingFeatureSteps.scala
+++ src/test/scala/testLab/steps/ShoppingFeatureSteps.scala
@@ -11,26 +11,25 @@ class ShoppingFeatureSteps extends ScalaDsl with EN:
   var warehouse: BasicWarehouse = _
   var cart: BasicCart = _
   var catalog: BasicCatalog = _
-  var item: Item = _
 
   Given("""^a shopping with (\d+) p1 products that costs (\d+)$""") { (n: Int, p: Int) =>
-    /*warehouse = new BasicWarehouse()
+    warehouse = new BasicWarehouse()
     p1 = new Product("Hat")
     warehouse.supply(p1, n)
     catalog = new BasicCatalog(Map[Product, Price](
       p1 -> new Price(p)
     ))
     cart = new BasicCart()
-    shopping = new Shopping(warehouse, catalog, cart, new BasicLogger(">> "))*/
+    shopping = new Shopping(warehouse, catalog, cart, new BasicLogger(">> "))
 
   }
 
   When("""^buy (\d+) item of p1$""") { (qt: Int) =>
-    //shopping.pick(p1, qt)
+    shopping.pick(p1, qt)
   }
 
   Then("""^I have to pay (\d+)$""") { (expected: Int) =>
-    //Assertions.assertEquals(expected, cart.totalCost)
+    Assertions.assertEquals(expected, cart.totalCost)
   }
 
 
